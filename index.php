fc770ff93e99692bd3f172f7d74e89f4bb8132fc
tracnghiem <huuthang9764@gmail.com>
2021-04-25 23:48:34 +0700

    Add

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="public/frontend/img/logo-02.ico" type="image/gif" sizes="16x16">
+  <title>Hệ thống thi trắc nghiệm trực tuyến</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="public/frontend/css/site.css" rel="stylesheet">
+  <link href="public/frontend/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<header id="main-header">
+  <div style="min-height:7px; background-image:linear-gradient(to top, #dbdbdb, #ffffff);">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <a href="#" style="text-decoration:none">
+            <img src="public/frontend/img/logo-02.ico" alt="Logo"  width="50" height="30"style="float:left; margin-right:10px; min-height:45px; margin-top:10px">
+            <p style="margin:0; color:#920009; font-size:13px; line-height:1.38; text-transform:uppercase; margin-top:14px; margin-bottom:3px">Khoa Công Nghệ Thông Tin</p>
+            <h4 class="top-header-caption">Hệ thống thi trắc nghiệm trực tuyến</h4>
+          </a>
+        </div>
+        <div class="col-md-6 text-right" style="margin-bottom:10px">
+          <a href="#" style="margin-top:13px;margin-bottom:10px; border-radius:5px; background-color:#124e7a; border-color:#124e7a; color:#fff; font-size:17px" class="btn btn-primary sign-in-pc">
+            <i class="fas fa-user-circle" style="color:#fff; margin-right:5px"></i> Đăng nhập
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav id="main-nav" class="navbar navbar-expand-lg navbar-toggleable-lg navbar-light border-bottom box-shadow mb-3"style="background: #124e7a; ">
+    <div class="container" >
+      <div class="navbar-collapse collapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" style="color:#fff" href="#">Home</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" style="color:#fff" href="#">Thi chính thức</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" style="color:#fff" href="#">Thi Thử</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" style="color:#fff" href="#" >Luyện tập</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="/tin-tuc">Tin tức</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+  <!-- Page Content -->
+<!-- <div style="background-color:#f5f5f6; padding-bottom:10px;">
+  @yield('content')
+</div> -->
+
+
+  <!-- Footer -->
+<footer class="border-top footer text-muted" id="footerD">
+  <div class="text-center" style="background-color:#f5f5f4; color:#124e7a; line-height:1.46; padding:15px 0px; font-size:13px">
+    <h4 style="text-transform:uppercase; font-size:15px; font-weight:bold">Trường ĐH Công nghệ Sài Gòn</h4>
+    Địa chỉ: 180 Cao Lỗ, Phường 4, Quận 8, Tp.Hồ Chí Minh<br>
+    SĐT: 039406964<br>
+    Email: huuthang9764@gmai.com.
+  </div>
+  <div class="text-center border-top" style="background-color:#1e5c8b; color:rgba(255, 255, 255, 0.8); padding:15px 5px; font-size:13px; font-weight:300">
+    Copyright © 2020 Hệ thống thi trắc nghiệm trực tuyến.<br>
+    Designed by <span><a target="_blank" href="#">HTStudy</a> <br>
+  </span></div>
+ <div class="icontop" id="icontop" style="display: none;">
+    <a href="#top"><i class="fas fa-arrow-up gototop"></i></a>
+  </div>
+</footer>
+
+  <!-- Bootstrap core JavaScript -->
+  
+  <script src="public/frontend/js/jquery-3.5.1.min.js"></script>
+  <script src="public/frontend/js/bootstrap.bundle.min.js"></script>
+  
+
+</body>
+
+</html>
